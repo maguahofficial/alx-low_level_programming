7fb4bdd1cee5d30e10628a1a7c5eed700dad8fc4
maguah <maguahofficial@gmail.com>
2023-09-08 22:48:54 +0300

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		printf("%d", d);
+	}
+	putchar('\n');
+	return (0);
+}
