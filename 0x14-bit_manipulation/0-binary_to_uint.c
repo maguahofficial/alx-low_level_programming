a190860f4a3b7d3e8ae20ce42a5277f774899b85
maguah <maguahofficial@gmail.com>
2023-10-27 03:12:42 +0300

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,12 +6,6 @@
  * the 0-binary_to_uint.c Bit manipulation task
  * Return: returns the converted number
  */
-/**
- * binary_to_uint - function converts a binary number to an unsigned int.
- * @b: pointer to a string containing a binary number
- *
- * Return: unsigned int with decimal value of binsry number, or 0 if error
- */
 unsigned int binary_to_uint(const char *b)
 {
 	int aintvarble;
